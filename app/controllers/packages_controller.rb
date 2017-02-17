21c168e43c92922748cb05f18c67b971b7d4eeec
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-16 21:41:50 -0800

    Add packages controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,2 +1,30 @@
 class PackagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action except: [:index] do
+    redirect_to :new_user_session_path unless current_user && current_user.admin?
+  end
+
+  def index
+    @packages = Package.all
+  end
+
+  def new
+    @package = Package.new
+  end
+
+  def create
+    @package = Package.new(package_params)
+    if @package.save
+      flash[:notice] = "Package successfully added."
+      redirect_to packages_path
+    else
+      flash[:alert] = "There were some problems with your submission."
+      render :new
+    end
+  end
+
+private
+  def package_params
+    params.require(:package).permit(:name, :description, :price, :image)
+  end
 end
