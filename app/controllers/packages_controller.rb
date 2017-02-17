c575de3ed0f9183eefb901e6f3beeae14398961f
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-16 21:48:10 -0800

    Admin privleges for new packages

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,7 +1,7 @@
 class PackagesController < ApplicationController
   before_action :authenticate_user!
   before_action except: [:index] do
-    redirect_to :new_user_session_path unless current_user && current_user.admin?
+    redirect_to new_user_session_path unless current_user && current_user.admin?
   end
 
   def index
