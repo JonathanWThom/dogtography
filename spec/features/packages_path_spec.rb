ca781e143d77a72d2680e96a2edff363a04edee2
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-17 11:27:49 -0800

    Packages controller integration full coverage

--- spec/features/packages_path_spec.rb
+++ spec/features/packages_path_spec.rb
@@ -26,4 +26,33 @@ describe 'the packages path' do
     visit package_path(package)
     expect(page).to have_content("Image")
   end
+
+  it 'will let admin edit a package' do
+    user = create(:user, :admin => true)
+    login_as(user)
+    package = create(:package)
+    visit edit_package_path(package)
+    fill_in "Name", :with => "New Package Name"
+    click_on "Update Package"
+    expect(page).to have_content("Photo package was successfully updated")
+  end
+
+  it 'will fail to edit a package' do
+    user = create(:user, :admin => true)
+    login_as(user)
+    package = create(:package)
+    visit edit_package_path(package)
+    fill_in "Name", :with => ""
+    click_on "Update Package"
+    expect(page).to have_content("errors")
+  end
+
+  it 'will let admin destroy a package' do
+    user = create(:user, :admin => true)
+    login_as(user)
+    package = create(:package)
+    visit package_path(package)
+    click_on 'Delete This Package'
+    expect(page).to have_content("Welcome to Dogtography")
+  end
 end
