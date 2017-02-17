17344e971a7d7524bb772003cddcf0eeebb132d1
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-17 08:16:43 -0800

    Wil create new images

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,5 +1,5 @@
 class PackagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action except: [:index] do
     redirect_to new_user_session_path unless current_user && current_user.admin?
   end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,4 +1,7 @@
 class Package < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "images/placeholder.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   validates :name, :description, :price, :presence => true
   has_many :reviews
   has_many :users, through: :reviews
