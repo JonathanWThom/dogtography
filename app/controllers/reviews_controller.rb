3e16d656ee4f1950897ca3fbd18800d0fe8e603e
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-17 10:10:14 -0800

    Can delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,10 @@
 class ReviewsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action only: [:destroy] do
+    redirect_to new_user_session_path unless current_user && current_user.admin?
+  end
+
+
   def new
     @package = Package.find(params[:id])
     @review = @package.reviews.new
@@ -12,8 +17,16 @@ class ReviewsController < ApplicationController
       flash[:notice] = "Review successfully submitted."
       redirect_to package_path(@package)
     else
+      flash[:alert] = "There were some problems with your submission."
       redirect_to package_path(@package)
     end
+  end
+
+  def destroy
+    @package = Package.find(params[:package_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to package_path(@package)
   end
 
 private
